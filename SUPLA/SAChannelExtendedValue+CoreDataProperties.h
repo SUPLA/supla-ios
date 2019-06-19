77f169096d8f32b2312185f2a995b13f17e785e7
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-06-19 12:51:42 +0200

    SAChannelExtendedValue properties changed

--- SUPLA/SAChannelExtendedValue+CoreDataProperties.h
+++ SUPLA/SAChannelExtendedValue+CoreDataProperties.h
@@ -1,20 +1,11 @@
-/*
- Copyright (C) AC SOFTWARE SP. Z O.O.
- 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+//
+//  SAChannelExtendedValue+CoreDataProperties.h
+//  SUPLA
+//
+//  Created by Przemek Zygmunt on 19/06/2019.
+//  Copyright © 2019 AC SOFTWARE SP. Z O.O. All rights reserved.
+//
+//
 
 #import "SAChannelExtendedValue+CoreDataClass.h"
 
@@ -25,11 +16,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (NSFetchRequest<SAChannelExtendedValue *> *)fetchRequest;
 
-@property (nullable, nonatomic, retain) NSData *data;
+@property (nullable, nonatomic, retain) NSObject *content;
 @property (nonatomic) int32_t type;
-@property (nullable, nonatomic, retain) SAChannelElectricityMeterValue *emval;
-@property (nullable, nonatomic, retain) SAChannelImpulseCounterValue *icval;
-@property (nullable, nonatomic, retain) SAChannelThermostatValue *thval;
+@property (nonatomic) int32_t channel_id;
 
 @end
 
--- SUPLA/SAChannelExtendedValue+CoreDataProperties.m
+++ SUPLA/SAChannelExtendedValue+CoreDataProperties.m
@@ -1,20 +1,11 @@
-/*
- Copyright (C) AC SOFTWARE SP. Z O.O.
- 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+//
+//  SAChannelExtendedValue+CoreDataProperties.m
+//  SUPLA
+//
+//  Created by Przemek Zygmunt on 19/06/2019.
+//  Copyright © 2019 AC SOFTWARE SP. Z O.O. All rights reserved.
+//
+//
 
 #import "SAChannelExtendedValue+CoreDataProperties.h"
 
@@ -24,10 +15,8 @@
 	return [NSFetchRequest fetchRequestWithEntityName:@"SAChannelExtendedValue"];
 }
 
-@dynamic data;
+@dynamic content;
 @dynamic type;
-@dynamic emval;
-@dynamic icval;
-@dynamic thval;
+@dynamic channel_id;
 
 @end
