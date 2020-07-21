71ed9fd5aca8b00ab298d343c547dc5cc06c546a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-07-21 17:42:19 +0200

    Properties moved from .h to .m

--- SUPLA/SAChannelStatePopup.m
+++ SUPLA/SAChannelStatePopup.m
@@ -17,6 +17,37 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #import "SAChannelStateExtendedValue.h"
 
 @interface SAChannelStatePopup ()
+@property (weak, nonatomic) IBOutlet UIButton *btnClose;
+@property (weak, nonatomic) IBOutlet UILabel *lTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lIPTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lIP;
+@property (weak, nonatomic) IBOutlet UILabel *lMACTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lMAC;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryLevelTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryLevel;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryPoweredTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryPowered;
+@property (weak, nonatomic) IBOutlet UILabel *lWifiRSSITitle;
+@property (weak, nonatomic) IBOutlet UILabel *lWifiRSSI;
+@property (weak, nonatomic) IBOutlet UILabel *lWifiSignalStrengthTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lWifiSignalStrength;
+@property (weak, nonatomic) IBOutlet UILabel *lBridgeNodeOnlineTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lBridgeNodeOnline;
+@property (weak, nonatomic) IBOutlet UILabel *lBridgeNodeSignalStrengthTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lBridgeNodeSignalStrength;
+@property (weak, nonatomic) IBOutlet UILabel *lUptimeTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lUptime;
+@property (weak, nonatomic) IBOutlet UILabel *lConnectionUptimeTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lConnectionUptime;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryHealthTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lBatteryHealth;
+@property (weak, nonatomic) IBOutlet UILabel *lConnectionResetCauseTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lConnectionResetCause;
+@property (weak, nonatomic) IBOutlet UILabel *lLightsourceLifespanTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lLightsourceLifespan;
+@property (weak, nonatomic) IBOutlet UILabel *lLightsourceOperatingTimeTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lLightsourceOperatingTime;
+@property (weak, nonatomic) IBOutlet UIView *vList;
 
 @end
 
