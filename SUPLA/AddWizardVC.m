96984e8410163e1a2c48647fa086d451a37be291
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-12-21 13:27:24 +0100

    joinOnce = NO

--- SUPLA/AddWizardVC.m
+++ SUPLA/AddWizardVC.m
@@ -579,7 +579,7 @@
             _blinkTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(blinkTimerFireMethod:) userInfo:nil repeats:YES];
             
             if ([SAWifiAutoConnect isAvailable]) {
-                self.swAutoMode.on = YES;
+                self.swAutoMode.on = NO;
                 self.swAutoMode.hidden = NO;
                 self.lAutoMode.hidden = NO;
             } else {
