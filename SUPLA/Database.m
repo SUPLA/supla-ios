cc0bff3027ee4b7706a33e3105974dd6be7b5620
pki-acs <pki-acs@ex.com.pl>
2022-02-13 12:35:42 +0100

    revert core data thread model changes

--- SUPLA/Database.m
+++ SUPLA/Database.m
@@ -133,7 +133,7 @@ again:
     if (!coordinator) {
         return nil;
     }
-    _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    _managedObjectContext = [[NSManagedObjectContext alloc] init];
     [_managedObjectContext setPersistentStoreCoordinator:coordinator];
     [_managedObjectContext setMergePolicy:NSMergeByPropertyStoreTrumpMergePolicy];
     return _managedObjectContext;
@@ -183,11 +183,8 @@ again:
     }
     
     [fetchRequest setEntity:[NSEntityDescription entityForName:en inManagedObjectContext: self.managedObjectContext]];
-    __block NSError *error = nil;
-    __block NSArray *r;
-    [self.managedObjectContext performBlockAndWait: ^{
-        r = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
-    }];
+    NSError *error = nil;
+    NSArray *r = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
     
     if ( error == nil && r.count > 0 ) {
         return r;
@@ -221,12 +218,8 @@ again:
     [fetchRequest setEntity:[NSEntityDescription entityForName:en inManagedObjectContext: self.managedObjectContext]];
     [fetchRequest setIncludesSubentities:NO];
     
-    __block NSError *fetchError = nil;
-    __block NSUInteger count;
-    
-    [self.managedObjectContext performBlockAndWait:^{
-        count = [self.managedObjectContext countForFetchRequest:fetchRequest error:&fetchError];;
-    }];
+    NSError *fetchError = nil;
+    NSUInteger count = [self.managedObjectContext countForFetchRequest:fetchRequest error:&fetchError];
     
     if(count == NSNotFound || fetchError != nil ) {
         count = 0;
@@ -618,11 +611,7 @@ again:
 }
 
 -(BOOL) setAllOfChannelVisible:(int)visible whereVisibilityIs:(int)wvi {
-    __block BOOL rv;
-    [self.managedObjectContext performBlockAndWait:^{
-        rv = [self setAllItemsVisible:visible whereVisibilityIs:wvi entityName:@"SAChannel"];
-    }];
-    return rv;
+    return [self setAllItemsVisible:visible whereVisibilityIs:wvi entityName:@"SAChannel"];
 }
 
 -(NSUInteger) getChannelCount {
