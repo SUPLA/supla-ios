97f09f7b69ab159311a133e0660e6a21afe1087f
Michal Polanski <michal@polanscy.com.pl>
2024-12-12 23:19:49 +0100

    Make awaiting timeout in tests longer

--- SUPLATests/Tools/Extensions/XCTestCaseExtensions.swift
+++ SUPLATests/Tools/Extensions/XCTestCaseExtensions.swift
@@ -55,7 +55,7 @@ extension XCTestCase {
             testcase()
         }
         
-        waitForExpectations(timeout: 2) { _ in
+        waitForExpectations(timeout: 5) { _ in
             // assert
             XCTAssertEqual(assertionMessage, expectedMessage)
             
