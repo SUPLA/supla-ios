f0274f16d8c53aa2a51552fed5fe8c1875f150fa
Michal Polanski <michal@polanscy.com.pl>
2026-08-13 10:29:05 +0200

    Migrate Valve, Container, Gate and Recuperator detail to SwiftUI Navigation

---
+++ SUPLA/Features/Details/ValveDetail/General/ValveGeneralScreen.swift
@@ -0,0 +1,44 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+import SwiftUI
+
+extension ValveGeneralFeature {
+    struct Screen: SwiftUI.View {
+        @StateObject private var viewModel: ViewModel
+        @StateObject private var stateDialogViewModel = StateDialogFeature.ViewModel()
+        @StateObject private var captionChangeDialogViewModel = CaptionChangeDialogFeature.ViewModel()
+
+        init(itemBundle: ItemBundle) {
+            _viewModel = StateObject(wrappedValue: ViewModel(itemBundle: itemBundle))
+        }
+
+        var body: some SwiftUI.View {
+            SuplaCore.ViewModelHost(viewModel) { state in
+                ValveGeneralFeature.View(
+                    viewState: state,
+                    stateDialogViewModel: stateDialogViewModel,
+                    captionChangeDialogViewModel: captionChangeDialogViewModel,
+                    delegate: viewModel,
+                    onInfoClick: { stateDialogViewModel.show(remoteId: $0.id) },
+                    onCaptionLongPress: { captionChangeDialogViewModel.show(sensorData: $0) }
+                )
+            }
+        }
+    }
+}
